2ac2d339116e672465d46035b00da30dd8fc938b
Trevor Jones <trevordavidjones@gmail.com>
2017-01-28 19:31:25 -0500

    add acronym exercise

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -5,5 +5,13 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    Regex.scan(~r{[A-Z]?[a-z]*}, string)
+    |> List.flatten()
+    |> Enum.map(&(first_letter(&1)))
+    |> Enum.join("")
+  end
+
+  defp first_letter(word) do
+    String.first(String.upcase(word))
   end
 end
